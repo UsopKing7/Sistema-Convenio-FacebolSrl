37666ee7bd82207879560ba5f8ed42ab9fea24a6
UsopKing7 <programadorusop@gmail.com>
2025-03-14 19:18:47 -0400

    traduccion de ingles a la base de datos

--- sql/tablas.sql
+++ sql/tablas.sql
@@ -3,23 +3,22 @@ USE agreement_system;
 
 CREATE TABLE companies (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  nombre varchar(100) NOT NULL,
-  gmail varchar(100) UNIQUE NOT NULL,
+  name varchar(100) NOT NULL,
+  email varchar(100) UNIQUE NOT NULL,
   phone varchar(20),
-  direccion TEXT,
-  passwd varchar(255) NOT NULL,
-  estado ENUM('activo', 'inactivo') DEFAULT 'activo',
-  creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  address TEXT,
+  password varchar(255) NOT NULL,
+  state BOOLEAN DEFAULT TRUE,
+  registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 ); 
 
 CREATE TABLE users (
   id INT KEY AUTO_INCREMENT,
   companies_id INT NOT NULL,
-  nombre varchar(100) NOT NULL,
+  name varchar(100) NOT NULL,
   email varchar(100) UNIQUE NOT NULL,
-  passwd varchar(255) NOT NULL,
-  rol ENUM('admin', 'empleado') DEFAULT 'empleado',
-  creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  password varchar(255) NOT NULL,
+  rol TEXT,
+  registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (companies_id) REFERENCES companies(id) ON DELETE CASCADE
 );
-
