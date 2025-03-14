5bd8f57d9121808fc304a432c80e5ad65d95eb16
UsopKing7 <programadorusop@gmail.com>
2025-03-13 23:01:22 -0400

    configuradno el server

--- src/server.js
+++ src/server.js
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
 
+import express from 'express'
+import { midelware } from './routes/midelware.js';
+
+const app = express()
+app.use(express.json())
+
+app.use(midelware)
+
+const PORT = process.env.PORT ?? 3333
+app.listen(PORT, () => {
+  console.table({
+    URL:  'http://localhost:' + PORT
+  })
+})
