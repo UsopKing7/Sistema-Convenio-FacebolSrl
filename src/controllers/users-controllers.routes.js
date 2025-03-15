c70676ea8814b3353430054c44e8fe797d5b2a5d
UsopKing7 <programadorusop@gmail.com>
2025-03-14 22:38:02 -0400

    controlers user mas limpio

--- src/controllers/users-controllers.routes.js
+++ src/controllers/users-controllers.routes.js
@@ -13,11 +13,10 @@ usersRouter.post('/', async (req, res) => {
     const hashedPassword = await bcrypt.hash(newUsers.password, 10)
 
     const [company] = await pool.query('SELECT id FROM companies WHERE id = ?', [newUsers.companies_id])
-    if (company.length === 0) {
-      return res.status(400).json({ error: 'the company is not registered'})
-    }
 
-    const [data] = await pool.query('INSERT INTO users (companies_id, name, email, password, rol) VALUES (?, ?, ?, ?, ?)',
+    if (company.length === 0) return res.status(404).json({ message: 'company not found "400"'})
+
+    const [data] = await pool.query('INSERT INTO users (companies_id, name, email, password, rol) VALUES (?, ?, ?, ?, ?)', 
       [
         newUsers.companies_id,
         newUsers.name,
@@ -27,8 +26,8 @@ usersRouter.post('/', async (req, res) => {
       ]
     )
 
-    res.status(201).json({ message: 'Usuario registrado con exito', userId: data.insertId})
+    res.status(201).json({ message: 'successfully registered company', userID: data.insertId})
   } catch (error) {
-    return res.status(500).json({ error: 'error en el servidor', error})
+    return res.status(500).json({ error: 'internal server error "500"', details: error.errors ?? error.message })
   }
 })
