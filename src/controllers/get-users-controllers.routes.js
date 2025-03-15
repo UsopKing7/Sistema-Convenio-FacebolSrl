2f178603c55311a85018073e9fd3efbf0e3e4b38
UsopKing7 <programadorusop@gmail.com>
2025-03-15 05:20:26 -0400

    GET user adn companies

---
+++ src/controllers/get-users-controllers.routes.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import { Router } from 'express'
+import { pool } from '../models/db.js'
+
+export const getUsers = Router()
+
+getUsers.get('/', async(req, res) => {
+  try {
+    const [users] = await pool.query('SELECT * FROM users')
+    return res.status(200).json({ users })
+  } catch (error) {
+    return res.status(500).json({ message: 'error in the server', error: error.message})
+  }
+})
