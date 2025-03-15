98753c1fa5ce52b6f2c227e716c8a32dfbe847e0
UsopKing7 <programadorusop@gmail.com>
2025-03-14 22:10:22 -0400

    ajuste para la ruta del controller al server

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
 import express from 'express'
-import { midelware } from './routes/midelware.js';
+import { middleware } from './routes/middleware.js';
 
 const app = express()
 app.use(express.json())
 app.disable('x-powered-by')
 
-app.use(midelware)
+app.use(middleware)
 
-const PORT = process.env.PORT ?? 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => {
   console.table({
     URL:  'http://localhost:' + PORT
