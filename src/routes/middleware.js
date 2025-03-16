bd7e0d85138496aef90bec0225056b469c106b5c
UsopKing7 <programadorusop@gmail.com>
2025-03-15 20:48:50 -0400

    router para el CRUD

--- src/routes/middleware.js
+++ src/routes/middleware.js
@@ -6,6 +6,9 @@ import { deleteUsers } from '../controllers/delete-users-controllers.routes.js'
 import { deleteCompanies } from '../controllers/delete-companies-controllers.routes.js'
 import { getCompanies } from '../controllers/get-companies-controllers.routes.js'
 import { getUsers } from '../controllers/get-users-controllers.routes.js'
+import { PatchRouterCompanies } from '../controllers/patch-companies-controllers.routes.js'
+import { PatchRouterUsers } from '../controllers/patch-users-controllers.routes.js'
+
 import { Router } from 'express'
 
 export const middleware = Router()
@@ -17,3 +20,6 @@ middleware.use('/users', getUsers)
 middleware.use('/users/delete', deleteUsers)
 middleware.use('/companies', getCompanies)
 middleware.use('/companies/delete', deleteCompanies)
+
+middleware.use('/companies/update', PatchRouterCompanies)
+middleware.use('/users/update', PatchRouterUsers)
