ac43ef826ec3e9cbf851510c9b147c36d0d02098
UsopKing7 <programadorusop@gmail.com>
2025-03-23 04:48:55 -0400

    server configuration

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 #!/usr/bin/env node
 
 import express from 'express'
-import { middleware } from './routes/middleware.js';
+import { middleware } from './routes/middleware.js'
+import cookieParser from 'cookie-parser';
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 app.disable('x-powered-by')
+app.set('view engine', 'ejs')
+app.set('views', './src/views')
+app.use(express.urlencoded({ extended: true }));
 
 app.use(middleware)
+
+app.get('/', (req, res) => {
+  res.render('login');
+});
 
 const PORT = process.env.PORT || 3333
 app.listen(PORT, () => {
