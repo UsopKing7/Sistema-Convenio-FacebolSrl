deeea54da935cebca9747e8fc5c3c8d735d1f1e2
UsopKing7 <programadorusop@gmail.com>
2025-04-18 22:18:01 -0400

    agregando ruta sucursales

--- src/routes/middleware.js
+++ src/routes/middleware.js
@@ -1,8 +1,11 @@
-
+import { Router } from 'express'
 import { routerSucursales } from '../controllers/sucursales/sucursal.routes.js'
+import { routerRegiste } from '../controllers/login/login.routes.js'
 
-middleware.use("/sucursal", routerSucursales)
+export const middleware = Router()
 
+middleware.use('/sucursal', routerSucursales)
+middleware.use('/', routerRegiste)
 
 middleware.use((req, res) => {
   console.log(req.url)
