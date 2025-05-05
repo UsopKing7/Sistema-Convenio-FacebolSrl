cc5eae8eab2307a856eb2bb1c58e2260ef9409b9
UsopKing7 <programadorusop@gmail.com>
2025-05-05 01:20:14 -0400

    mejora de registro de usuarios

--- backend/src/controllers/login/login.routes.js
+++ backend/src/controllers/login/login.routes.js
@@ -2,7 +2,12 @@
 
 import { Router } from 'express'
 import { pool, SECRET_JWK_KEY } from '../../models/db.js'
-import { schemaRegister, schemaLogin } from '../../routes/SchemaRegister.js'
+import {
+  schemaRegister,
+  schemaLogin,
+  schemaRoles,
+  schemaPermiso
+} from '../../routes/SchemaRegister.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
@@ -13,27 +18,35 @@ routerRegiste.post('/login', async (req, res) => {
     const vLogin = schemaLogin.parse(req.body)
 
     const [empresa] = await pool.query(
-      'SELECT * FROM usuarios WHERE correo = ?', [vLogin.correo]
+      'SELECT * FROM usuarios WHERE correo = ?',
+      [vLogin.correo]
     )
 
     if (empresa.length === 0) {
       return res.status(404).json({
-        message: 'No existe una empresa con ese correo'
+        message: 'No existen usuarios con este correo'
       })
     }
 
-    const isPasswordValid = await bcrypt.compare(vLogin.contrasena, empresa[0].contrasena)
+    const isPasswordValid = await bcrypt.compare(
+      vLogin.contrasena,
+      empresa[0].contrasena
+    )
     if (!isPasswordValid) {
       return res.status(401).json({
         message: 'Usuario o contraseña incorrectos'
       })
     }
 
-    const token = jwt.sign({
-      id: empresa[0].id,
-      nombre_empresa: empresa[0].nombre_empresa,
-      correo: empresa[0].correo
-    }, SECRET_JWK_KEY, { expiresIn: '1h' })
+    const token = jwt.sign(
+      {
+        id: empresa[0].id,
+        nombre_empresa: empresa[0].nombre_empresa,
+        correo: empresa[0].correo
+      },
+      SECRET_JWK_KEY,
+      { expiresIn: '1h' }
+    )
 
     res.cookie('token', token, {
       httpOnly: true,
@@ -57,22 +70,46 @@ routerRegiste.post('/login', async (req, res) => {
 routerRegiste.post('/register', async (req, res) => {
   try {
     const schemaRegistro = schemaRegister.parse(req.body)
+    const validacionRoles = schemaRoles.parse(req.body)
+    const validacionPermiso = schemaPermiso.parse(req.body)
+
     const hashPassword = await bcrypt.hash(schemaRegistro.contrasena, 10)
+
+    const [dataRoles] = await pool.query(
+      'INSERT INTO roles (nombre_rol, descripcion_rol) VALUES (?,?)',
+      [validacionRoles.nombre_rol, validacionRoles.descripcion_rol]
+    )
+
+    const idRoles = dataRoles.insertId
+
+    const [dataPermisos] = await pool.query(
+      'INSERT INTO permisos (nombre) VALUES (?)',
+      [validacionPermiso.nombre]
+    )
+
+    const idPermiso = dataPermisos.insertId
+
     await pool.query(
-      'INSERT INTO empresas (nombre_empresa, representante, celular, correo, descripcion, nit, contrasena) VALUES (?,?,?,?,?,?,?)',
+      'INSERT INTO roles_permisos (permiso_id, rol_id) VALUES (?,?)',
+      [idPermiso, idRoles]
+    )
+
+    await pool.query(
+      'INSERT INTO usuarios (nombre, correo, telefono, contrasena, rol_id) VALUES (?,?,?,?,?)',
       [
-        schemaRegistro.nombre_empresa,
-        schemaRegistro.representante,
-        schemaRegistro.celular,
+        schemaRegistro.nombre,
         schemaRegistro.correo,
-        schemaRegistro.descripcion,
-        schemaRegistro.nit,
-        hashPassword
+        schemaRegistro.telefono,
+        hashPassword,
+        idRoles
       ]
     )
+
     res.status(201).json({
-      message: 'Empresa registrada con exito',
-      empresa: schemaRegistro.nombre_empresa
+      message: 'Registro Exitoso',
+      nombre: schemaRegistro.nombre,
+      telefono: schemaRegistro.telefono,
+      correo: schemaRegistro.correo
     })
   } catch (error) {
     return res.status(500).json({
